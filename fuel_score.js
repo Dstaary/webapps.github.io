9189ac72937992157bbafe0b81aa1d5f8781781e
Dstaary <51885195+Dstaary@users.noreply.github.com>
2021-01-27 18:32:06 +0100

    Update fuel_score.js
    upgrade function

--- fuel_score.js
+++ fuel_score.js
@@ -2,25 +2,32 @@ const dystans = document.querySelector('input.dystans');
 const spalanie = document.querySelector('input.spalanie');
 const cenaPaliwa = document.querySelector('input.CenaPaliwa');
 const iloscOsob = document.querySelector('input.IloscOsob');
-
-const spanSpalanie = document.getElementById("spalanieScore");
-const spanOsoby = document.getElementById("osoby");
-const spanKoszt = document.getElementById("koszt");
-const spanZrzutka = document.getElementById("zrzutka");
+const formLabels = document.querySelectorAll(".check_value");
 
 document.getElementById('fuelBtn').addEventListener("click", () => {
+    const div = document.querySelector(".fuel_info");
+    const newSpan = document.querySelector(".score");
+    const errorSpan = document.querySelector(".error_msg")
+
     const fuelL = (dystans.value * spalanie.value) / 100;
 
     const fuelScore = fuelL * cenaPaliwa.value;
     
     const zrzutka = fuelScore / iloscOsob.value;
 
-    spanSpalanie.textContent = spalanie.value;
-    spanOsoby.textContent = iloscOsob.value;
-    spanKoszt.textContent = fuelScore.toFixed(2);
-    spanZrzutka.textContent = zrzutka.toFixed(2);
+    for (const formLabel of formLabels) {
+        if (!formLabel.value){
+            errorSpan.innerHTML = `<p>Uzupełnij wszystkie pola !</p>`
+            return;
+        } else {
+            errorSpan.innerHTML = ``;
+        }
+    }
+
+    newSpan.innerHTML = `<p class="score">Przy średnim spalaniu: <b>${spalanie.value}</b>/100 km,<br> koszt podróży dla <b>${iloscOsob.value}</b></span> osób<br> wyniesie: <b>${fuelScore.toFixed(2)}</b> zł.<br> Zrzucacie się po: <b>${zrzutka.toFixed(2)}</b> zł</p>`
+
+    div.appendChild(newSpan);
 
 })
-
 
 
