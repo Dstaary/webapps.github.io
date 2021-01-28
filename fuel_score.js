f8e7d1c56e3542bccff87cde72a4e8a52ee2168d
Dstaary <51885195+Dstaary@users.noreply.github.com>
2021-01-28 18:13:32 +0100

    Update fuel_score.js

--- fuel_score.js
+++ fuel_score.js
@@ -4,30 +4,25 @@ const cenaPaliwa = document.querySelector('input.CenaPaliwa');
 const iloscOsob = document.querySelector('input.IloscOsob');
 const formLabels = document.querySelectorAll(".check_value");
 
-document.getElementById('fuelBtn').addEventListener("click", () => {
-    const div = document.querySelector(".fuel_info");
-    const newSpan = document.querySelector(".score");
-    const errorSpan = document.querySelector(".error_msg")
+const form = document.querySelector(".fuel_form");
+const newSpan = document.querySelector(".score");
 
-    const fuelL = (dystans.value * spalanie.value) / 100;
+form.addEventListener('input', () => {
 
-    const fuelScore = fuelL * cenaPaliwa.value;
-    
-    const zrzutka = fuelScore / iloscOsob.value;
-
-    for (const formLabel of formLabels) {
-        if (!formLabel.value){
-            errorSpan.innerHTML = `<p>Uzupełnij wszystkie pola !</p>`
-            return;
-        } else {
-            errorSpan.innerHTML = ``;
-        }
+for (const formLabel of formLabels) {
+    if (!formLabel.value){
+        return;
+    } else {
+        newSpan.innerHTML = "";       
     }
+}
 
-    newSpan.innerHTML = `<p class="score">Przy średnim spalaniu: <b>${spalanie.value}</b>/100 km,<br> koszt podróży dla <b>${iloscOsob.value}</b></span> osób<br> wyniesie: <b>${fuelScore.toFixed(2)}</b> zł.<br> Zrzucacie się po: <b>${zrzutka.toFixed(2)}</b> zł</p>`
+const fuelL = (dystans.value * spalanie.value) / 100;
 
-    div.appendChild(newSpan);
+const fuelScore = fuelL * cenaPaliwa.value;
 
+const zrzutka = fuelScore / iloscOsob.value;
+
+newSpan.innerHTML = `<p class="score">Przy średnim spalaniu: <b>${spalanie.value}</b>/100 km,<br> koszt podróży dla <b>${iloscOsob.value}</b></span> osób<br> wyniesie: <b>${fuelScore.toFixed(2)}</b> zł.<br> Zrzucacie się po: <b>${zrzutka.toFixed(2)}</b> zł</p>`
 })
-
 
