e7f73b1794ddfcafb81470f87207d4b5f7c7f8af
Dstaary <51885195+Dstaary@users.noreply.github.com>
2021-02-04 01:47:18 +0100

    Update fuel_score.js

--- fuel_score.js
+++ fuel_score.js
@@ -7,27 +7,23 @@ const formLabels = document.querySelectorAll(".check_value");
 const form = document.querySelector(".fuel_form");
 const newSpan = document.querySelector(".score");
 
-    form.addEventListener('input', () => {
+form.addEventListener('input', () => {
 
-        for (const formLabel of formLabels) {
-            if (!formLabel.value){
-                return;
-            } else {
-                if (formLabel.value < 0 || formLabel.value === "e") {
-                    errorSpan.innerHTML = "Niepoprawne dane lub puste pola !";
-                    return;
-                }  
-            }
-            newSpan.innerHTML = "";
-        }
-        
-        const fuelL = (dystans.value * spalanie.value) / 100;
-        
-        const fuelScore = fuelL * cenaPaliwa.value;
-        
-        const zrzutka = fuelScore / iloscOsob.value;
-        
-        errorSpan.innerHTML = "";
-        newSpan.innerHTML = `<p class="score">Przy średnim spalaniu: <b>${spalanie.value}</b>/100 km,<br> koszt podróży dla <b>${iloscOsob.value}</b></span> osób<br> wyniesie: <b>${fuelScore.toFixed(2)}</b> zł.<br> Zrzucacie się po: <b>${zrzutka.toFixed(2)}</b> zł</p>`
-        
+for (const formLabel of formLabels) {
+    if (!formLabel.value){
+        return;
+    } else {
+        newSpan.innerHTML = "";       
+    }
+}
+
+const fuelL = (dystans.value * spalanie.value) / 100;
+
+const fuelScore = fuelL * cenaPaliwa.value;
+
+const zrzutka = fuelScore / iloscOsob.value;
+
+newSpan.innerHTML = `<p class="score">Przy średnim spalaniu: <b>${spalanie.value}</b>/100 km,<br> koszt podróży dla <b>${iloscOsob.value}</b></span> osób<br> wyniesie: <b>${fuelScore.toFixed(2)}</b> zł.<br> Zrzucacie się po: <b>${zrzutka.toFixed(2)}</b> zł</p>`
 })
+
+
